aa59bda3bff4286aa5e1a2e6c990ff60259efc9e
Pawel <you@example.com>
2023-01-12 13:35:27 +0100

    custom user model in progress

--- estate-src/apps/users/admin.py
+++ estate-src/apps/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAadmin
+from django.utils.translation import gettext_lazy as _
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
+
+class UserAdmin(BaseUserAadmin):
+    ordering = []
