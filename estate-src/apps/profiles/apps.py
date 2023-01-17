280f2c90498e59c31781444dc30c8a232572691a
Pawel <you@example.com>
2023-01-17 14:26:40 +0100

    creating signals user-profile

--- estate-src/apps/profiles/apps.py
+++ estate-src/apps/profiles/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.profiles'
+
+    def ready(self):
+        """When the application is ready signals are import"""
+
+        from apps.profiles import signals
