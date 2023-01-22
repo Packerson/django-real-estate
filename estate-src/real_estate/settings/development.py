825e7e56e76536acd8c313a9062ec5bba401942e
Pawel <you@example.com>
2023-01-22 22:51:47 +0100

    configure sendrid

--- estate-src/real_estate/settings/development.py
+++ estate-src/real_estate/settings/development.py
@@ -1,5 +1,14 @@
 from .base import *
 
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+# EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = env("SENDGRID_API_KEY")
+DEFAULT_FROM_EMAIL = 'info@sharpmind.club'
+SITE_NAME = "Real Estate"
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 
 DATABASES = {
     'default': {
