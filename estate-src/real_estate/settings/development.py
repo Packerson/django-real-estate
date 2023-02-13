d5b29ff89c37b2f2604a24d82304ab6323ae3d99
Pawel <pawel.91.kaczmarek@gmail.com>
2023-02-13 16:03:12 +0100

    add apps :properties , enguiries

--- estate-src/real_estate/settings/development.py
+++ estate-src/real_estate/settings/development.py
@@ -1,14 +1,16 @@
 from .base import *
 
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = env("SENDGRID_API_KEY")
+
 SENDGRID_SANDBOX_MODE_IN_DEBUG = False
-DEFAULT_FROM_EMAIL = 'info@sharpmind.club'
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
-EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'
+SENDGRID_API_KEY = env("SENDGRID_API_KEY")
 EMAIL_PORT = env("EMAIL_PORT")
-
+DOMAIN = env("DOMAIN")
 
 SITE_NAME = "Real Estate"
 
