a1201edbf82fb409c7942b53d906ffcad4b2f850
Pawel <pawel.91.kaczmarek@gmail.com>
2023-04-17 22:17:08 +0200

    list properties, problem with display screen

--- estate-src/client/src/reducers/index.js
+++ estate-src/client/src/reducers/index.js
@@ -2,5 +2,5 @@ import {combineReducers} from "redux";
 import {propertyListReducer} from "./propertyReducers"
 
 export default combineReducers({
-    propertyListReducer: propertyListReducer,
+    propertiesList: propertyListReducer,
 });
