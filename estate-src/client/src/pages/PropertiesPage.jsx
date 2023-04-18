941bfcc0110b05cba03ae5f4e4e132a9fd500232
Pawel <pawel.91.kaczmarek@gmail.com>
2023-04-18 20:40:20 +0200

    fear(client: add property component)

--- estate-src/client/src/pages/PropertiesPage.jsx
+++ estate-src/client/src/pages/PropertiesPage.jsx
@@ -3,15 +3,20 @@ import {Row, Col, Container} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import Spinner from "../components/Spinner"
 import {getProperties} from '../features/properties/propertySlice'
-
+import { toast } from 'react-toastify'
+import Property from '../components/Property'
 
 const PropertiesPage = () => {
-    const { properties, isLoading, isSuccess } = useSelector(
+    const { properties, isLoading, isError, message } = useSelector(
         (state)=> state.properties);
     
     const dispatch = useDispatch();
 
     useEffect(()=>{
+        if (isError){
+            toast.error(message)
+        }
+        
         dispatch(getProperties());
     }, [dispatch]);
     
@@ -23,10 +28,33 @@ const PropertiesPage = () => {
    <>
     <Container>
         <Row>
-            <Col className='mg-top'>
-                <h1>Properties will be displayed here</h1>
+            <Col className='mg-top text-center'>
+                <h1>Our catalog of properties</h1>
+                <hr className='hr-text'/>
             </Col>
         </Row>
+
+        {
+            <>
+                <Row className='mt-3'>
+                    {properties.map((property)=>(
+                        <Col
+                        key={property.id}
+                        sm={12}
+                        md={6}
+                        lg={4}
+                        xl={3}
+                        >
+                            <Property property={property}/>
+                        
+                        </Col>
+                    )
+                    )}
+                </Row>
+            </>
+        }
+
+
     </Container>
    </>
   )
