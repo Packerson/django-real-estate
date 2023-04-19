05bd004f99e503357e42eff19bd090ec4033e4b3
Pawel <pawel.91.kaczmarek@gmail.com>
2023-04-19 14:26:57 +0200

    loginPage

--- estate-src/client/src/pages/PropertiesPage.jsx
+++ estate-src/client/src/pages/PropertiesPage.jsx
@@ -1,65 +1,59 @@
-import React, {useEffect} from 'react'
-import {Row, Col, Container} from 'react-bootstrap'
-import {useDispatch, useSelector} from 'react-redux'
-import Spinner from "../components/Spinner"
-import {getProperties} from '../features/properties/propertySlice'
-import { toast } from 'react-toastify'
-import Property from '../components/Property'
+import React, { useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Property from "../components/Property";
+import Spinner from "../components/Spinner";
 import Title from "../components/Title";
+import { getProperties } from "../features/properties/propertySlice";
 
 const PropertiesPage = () => {
-    const { properties, isLoading, isError, message } = useSelector(
-        (state)=> state.properties);
-    
-    const dispatch = useDispatch();
+	const { properties, isLoading, isError, message } = useSelector(
+		(state) => state.properties
+	);
 
-    useEffect(()=>{
-        if (isError){
-            toast.error(message)
-        }
-        
-        dispatch(getProperties());
-    }, [dispatch]);
-    
-    if (isLoading) {
-        return <Spinner/>;
-    }
+	const dispatch = useDispatch();
 
-  return (
-   <>
-   <Title title="Our Properties Catalog"/>
-    <Container>
-        <Row>
-            <Col className='mg-top text-center'>
-                <h1>Our catalog of properties</h1>
-                <hr className='hr-text'/>
-            </Col>
-        </Row>
+	useEffect(() => {
+		if (isError) {
+			toast.error(message, { icon: "😭" });
+		}
+		dispatch(getProperties());
+	}, [dispatch, isError, message]);
 
-        {
-            <>
-                <Row className='mt-3'>
-                    {properties.map((property)=>(
-                        <Col
-                        key={property.id}
-                        sm={12}
-                        md={6}
-                        lg={4}
-                        xl={3}
-                        >
-                            <Property property={property}/>
-                        
-                        </Col>
-                    )
-                    )}
-                </Row>
-            </>
-        }
+	if (isLoading) {
+		return <Spinner />;
+	}
+	return (
+		<>
+			<Title title="Our Properties Catalog" />
+			<Container>
+				<Row>
+					<Col className="mg-top text-center">
+						<h1>Our Catalog of properties</h1>
+						<hr className="hr-text" />
+					</Col>
+				</Row>
+				{
+					<>
+						<Row className="mt-3">
+							{properties.map((property) => (
+								<Col
+									key={property.id}
+									sm={12}
+									md={6}
+									lg={4}
+									xl={3}
+								>
+									<Property property={property} />
+								</Col>
+							))}
+						</Row>
+					</>
+				}
+			</Container>
+		</>
+	);
+};
 
-
-    </Container>
-   </>
-  )
-}
-
-export default PropertiesPage
+export default PropertiesPage;
